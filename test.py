3a0e0369c7c396091a2e7674b4e3cce8f02c385e
Asclepius-Med <453381299@qq.com>
2025-11-15 23:12:51 -0800

    update

---
+++ test.py
@@ -0,0 +1,73 @@
+import os
+
+# paper item
+
+
+  \resumeSubItem{WSI-LLaVA: A Multimodal Large Language Model for Whole Slide Image}
+      {\\
+      \scriptsize\textit{ Yuci Liang, Xinheng LYU, Meidan Ding, \textbf{Wenting Chen*\underline{(Corresponding author)}}, Xiaohan Xing, Jipeng Zhang, Sen Yang, Xiangjian He, Song Wu, Xiyue Wang, Linlin Shen}
+      \\
+      \textit{\textbf{ICCV} 2025}\textit{(International Conference on Computer Vision)} \href{https://arxiv.org/pdf/2412.02141}{(paper link)} \hfill (CCF A)}
+      
+      
+  \resumeSubItem{Medchain: Bridging the Gap Between LLM Agents and Clinical Practice through Interactive Sequential Benchmarking}
+      {\\
+      \scriptsize\textit{Jie Liu, Wenxuan Wang, Zizhan Ma, Guolin Huang, SU Yihang, Kao-Jung Chang, Haoliang Li, Linlin Shen, Michael Lyu, \textbf{Wenting Chen*\underline{(Corresponding author)}}}
+      \\
+      \textit{\textbf{NeurIPS} 2025} \textit{(Annual Conference on Neural Information Processing Systems)} \href{https://arxiv.org/pdf/2412.01605}{(paper link)} \hfill (CCF A)\\
+      \textit{(Spotlight)}
+      }
+      
+  \resumeSubItem{EndoBench: A Comprehensive Evaluation of Multi-Modal Large Language Models for Endoscopy Analysis}
+      {\\
+      \scriptsize\textit{Shengyuan Liu*, Boyun Zheng*, \textbf{Wenting Chen*\underline{(Co-first)}}, Zhihao Peng, Zhenfei Yin, Jing Shao, Jiancong Hu, Yixuan Yuan}
+      \\
+      \textit{\textbf{NeurIPS} 2025} \textit{(Annual Conference on Neural Information Processing Systems)} \href{https://arxiv.org/pdf/2505.23601}{(paper link)} \hfill (CCF A)\\
+      \textit{(Spotlight)}
+      }
+      
+      
+  \resumeSubItem{EAGLE: Expert-Guided Self-Enhancement for Preference Alignment in Pathology Large Vision-Language Model}
+      {\\
+      \scriptsize\textit{Meidan Ding, Jipeng Zhang, Haiqin Zhong, Xiaoqin Wang, \textbf{Wenting Chen*\underline{(Corresponding author)}}, Linlin Shen}
+      \\
+      \textit{\textbf{ACL} 2025} \textit{(Annual Meeting of the Association for Computational Linguistics)} \href{https://drive.google.com/file/d/1GdfPEI4Wx4o1LfkTs71wuDQTcmcmRoLa/view?usp=sharing}{(paper link)} \hfill (CCF A)}
+      
+  \resumeSubItem{Asclepius: A Spectrum Evaluation Benchmark for Medical Multi-Modal Large Language Models}
+      {\\
+      \scriptsize\textit{Jie Liu, Yihang Su, Jingyuan Huang, \textit{\textbf{Wenting Chen*\underline{(Corresponding author)}}, Yudi Zhang, Cheng-Yi Li, Kao-Jung Chang, Xiaohan Xing, Linlin Shen, Michael R. Lyu}}
+      \\
+      \textit{\textbf{ACL } 2025} \textit{(Annual Meeting of the Association for Computational Linguistics)} \href{https://arxiv.org/pdf/2402.11217}{(paper link)} \hfill (CCF A)}
+
+
+      \resumeSubItem{DAMPER: A Dual-Stage Medical Report Generation Framework with Coarse-Grained MeSH Alignment and Fine-Grained Hypergraph Matching}
+          {\\
+          \scriptsize\textit{Xiaofei Huang, \textit{\textbf{Wenting Chen*\underline{(Corresponding author)}}}, Jie Liu, Qisheng Lu, Xiaoling Luo, Linlin Shen}
+          \\
+          \textit{\textbf{AAAI} (The Association for the Advancement of Artificial Intelligence) 2025  \href{https://arxiv.org/pdf/2412.14535}{(paper link)}}\hfill (CCF A)}
+
+
+
+ \resumeSubItem{WSI-Agents: A Collaborative Multi-Agent System for Multi-Modal Whole Slide Image}
+      {\\
+      \scriptsize\textit{Xinheng Lyu, Yuci Liang, \textbf{Wenting Chen*\underline{(Corresponding author)}}, Meidan Ding, Jiaqi Yang, Huang Guolin, Daokun Zhang, Xiangjian He, Linlin Shen}
+      \\
+      \textit{\textbf{MICCAI} 2025 \textit{(International Conference on Medical Image Computing and Computer Assisted Intervention) }\\
+      (Early Accept; Acceptance rate: 9\%; Oral)
+      \href{https://drive.google.com/file/d/1T8aTBL_-JIZpKoRbvvYmoj2JisXssQMr/view?usp=sharing}{(paper link)}}
+      \hfill (CCF B)}
+
+  \resumeSubItem{Medical Image Synthesis via Fine-Grained Image-Text Alignment and Anatomy-Pathology Prompting}
+      {\\
+      \scriptsize\textit{\textbf{Wenting Chen}, Pengyu Wang, Hui Ren, Lichao Sun, Quanzheng Li, Yixuan Yuan, Xiang Li}
+      \\
+      \textit{\textbf{MICCAI} (International Conference on Medical Image Computing and Computer Assisted Intervention) 2024 \href{https://link.springer.com/chapter/10.1007/978-3-031-72390-2_23}{(paper link)}}  \hfill (CCF B)}
+
+      \resumeSubItem{GEM: Context-Aware Gaze EstiMation with Visual Search Behavior Matching for Chest Radiograph}
+          {\\
+          \scriptsize\textit{Shaonan Liu, \textit{\textbf{Wenting Chen*\underline{(Corresponding author)}}}, Jie Liu, Xiaoling Luo, Linlin Shen}
+          \\
+          \textit{\textbf{MICCAI} (International Conference on Medical Image Computing and Computer Assisted Intervention) 2024 \href{https://arxiv.org/abs/2408.05502}{(paper link)}} \hfill (CCF B)}
+
+
+
